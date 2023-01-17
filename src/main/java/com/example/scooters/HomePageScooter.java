61096421f2ec41f955ebc0f27e23ecb07b98378a
Andrew Alekseev <andrei.ig.alekseev@gmail.com>
2023-01-17 07:14:42 +0300

    ForDevelop

---
+++ src/main/java/com/example/scooters/HomePageScooter.java
@@ -0,0 +1,39 @@
+package com.example.scooters;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class HomePageScooter {
+
+
+
+    private final WebDriver driver;
+    private int index;
+
+    // Локатор для кнопки "Принять кукки"
+    private final By acceptCookie = By.className("App_CookieButton__3cvqF");
+    //  локатор для dropdown
+    private final By locator = By.id("accordion__panel-" + index);
+    //  локатор для панели
+    private final By panel = By.id("accordion__heading-" + index);
+    // метод отображения аккордиона после к клика
+    public Boolean isPanelVisible(int index) {
+        return driver.findElement(locator).isDisplayed();
+    }
+    public void clickAcceptCookie(){
+        driver.findElement(acceptCookie).click();
+    }
+
+    // метод клика на стрелку
+    public void clickPanel(int index) {
+        driver.findElement(locator).click();
+    }
+
+    public HomePageScooter(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open(){
+
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+}
